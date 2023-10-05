804a85d166d898c3d87d6fc41490f31fb1fc8ac1
mitshellmonyangionchiri <dbitnrb372323@spu.ac.ke>
2023-10-05 16:22:56 +0300

    about us

---
+++ index.php
@@ -0,0 +1,50 @@
+<h1>welcome</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/homepage.css">
+  <h1><i><b>TRENDY BOTIQUE</b></i></h1>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="signup.html">Signup</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <section>
+            <h2>About Us</h2>
+            <p>we are moving with todays trend in order to serve our customers at their comfort</p>
+        </section>
+
+        <section>
+            <h2>Contact Us</h2>
+            <p>You can reach us at <a href="trendy:botique@gmail.com">trendybotique@gmail.com</a>.</p>
+        </section>
+
+        <section>
+            <h2>login page</h2>
+            <p>sign up to our website by filling the form </p>
+            <div class="signup-container">
+                <h2>Sign Up</h2>
+                <form id="signup-form" action="redirect.html">
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" required>
+        
+                    <label for="password">Password:</label>
+                    <input type="password"id="password" name="password"minlength="8" required>
+        
+                    <button type="submit">Sign Up</button>
+            </section>
+    </main>
+    <footer>
+        <p><i>You dont have to be perfect but your outfit can be</i></p>
+    </footer>
+</body>
+</html>
